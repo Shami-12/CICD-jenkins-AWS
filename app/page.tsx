1d7edf37a94387c17424923971e188aba67802fb
Shami-12 <mahtisham220@gmail.com>
2025-10-16 20:54:45 +0500

    Me

--- app/page.tsx
+++ app/page.tsx
@@ -15,7 +15,7 @@ export default function Home() {
         />
         <ol>
           <li>
-            shami <code>app/page.tsx</code>.
+            shami malik <code>app/page.tsx</code>.
           </li>
           <li>Save and see your changes instantly.</li>
         </ol>
